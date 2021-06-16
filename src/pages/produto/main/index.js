f42f737ddabde8507d9d93cffbac0f10caede618
abc <abc@example.com>
2021-06-16 00:44:42 -0300

    A

--- src/pages/produto/main/index.js
+++ src/pages/produto/main/index.js
@@ -14,7 +14,7 @@ export default class Main extends Component {
     }
  
     componentDidMount() {
-        fetch("https://backend-produtoo.herokuapp.com/produto")
+        fetch("https://localhost:3003/produto")
             .then(produto =>
                 produto.json().then(produto => this.setState({ produto }))
             )

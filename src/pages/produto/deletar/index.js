7537aea9bf236a466359f6986c15de95bd1a5f7f
abc <abc@example.com>
2021-06-15 21:32:19 -0300

    Atualizadão

--- src/pages/produto/deletar/index.js
+++ src/pages/produto/deletar/index.js
@@ -29,7 +29,7 @@ class DeletarProduto extends Component {
     componentDidMount() {
         const { id } = this.props.match.params;
  
-        fetch(`https://backend-produtoo.herokuapp.com/produto`)
+        fetch(`https://backend-produtoo.herokuapp.com/produto${id}`)
             .then(data => {
                 data.json().then(data => {
                     if (data.error) {
@@ -71,7 +71,7 @@ class DeletarProduto extends Component {
     handleClick = event => {
         const { id } = this.props.match.params;
  
-        fetch(`https://backend-produtoo.herokuapp.com/produto`, {
+        fetch(`https://backend-produtoo.herokuapp.com/produto${id}`, {
             method: "delete"
         })
             .then(data => {
